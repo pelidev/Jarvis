237b1a683351e5fcf4a02fa866970b7a399d6822
pelidev <113928692+pelidev@users.noreply.github.com>
2025-08-15 20:44:24 -0400

    upcoming funct

--- combFunctions/openTasks.py
+++ combFunctions/openTasks.py
@@ -34,6 +34,35 @@ def incompletetasks(days):
 
     return tasks
 
+def upcomingtasks(days):
+    days += 1
+    days = range(days)
+    tasks = []
+    for i in days:
+        date_count_set = 0
+        now = datetime.now(ZoneInfo("America/Denver")) + timedelta(days=i)
+        month_folder = JOURNAL_BASE_DIR / now.strftime("%Y-%m")
+        file = month_folder / now.strftime("%Y-%m-%d.txt")
+
+        if not file.exists():
+            continue
+
+        with open(file, "r") as f:
+            lines = f.readlines()
+
+        for line in lines:
+            line = line.strip()
+            if line.startswith("_"):
+                if date_count_set == 0:
+                    tasks.append(now.strftime("%a %Y-%m-%d"))
+                    date_count_set = 1
+                tasks.append(line)
+            else:
+                pass
+
+
+    return tasks
+
 
 
 
---
+++ commandDictionary/upcoming_command.py
@@ -0,0 +1,25 @@
+from commandDictionary.command_struct import Command
+from combFunctions import openTasks
+import config
+from textAnimations.blockReveal import openType
+
+
+class past_incomplete_command(Command):
+    name = "upcoming"
+    aliases = ["upc"]
+
+    def execute(self, args):
+        dmod = 0
+        for arg in args:
+            try:
+                dmod = int(arg)
+                break
+            except ValueError:
+                pass
+
+        collection = openTasks.upcomingtasks(dmod)
+        for task in collection:
+            openType(task)
+
+
+        config.clearCheck = False
